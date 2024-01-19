fc90ef33592d83be8b9fbd11e2d01d1c4df832ad
deepak <deepakbaradwaj933@gmail.com>
2024-01-19 18:27:16 +0530

    a

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
     <link rel="stylesheet" href="./assets/css/home.css">
     <title>Goa.callgirlsinlucknow</title>
     <meta name="description" content="">
