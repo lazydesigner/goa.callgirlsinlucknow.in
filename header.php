9d0ff2a719705a4231508b0df1e0af00b5c64abf
deepak <deepakbaradwaj933@gmail.com>
2024-03-01 21:00:04 +0530

    initial commit

--- header.php
+++ header.php
@@ -13,13 +13,13 @@
         <nav>
             <ul id="menu">
                 <li id="li" class="active"><a href="<?=get_url() ?>"><button>Home</button></a></li>
-                <li id="li"><a href="https://www.callgirlsinlucknow.in/reviews/"><button>Reviews</button></a></li>
-                <li id="li"><a href="https://www.callgirlsinlucknow.in/gallery/"><button>Gallery</button></a></li>
-                <li id="li"><a href="https://www.callgirlsinlucknow.in/price-list/"><button>Price List</button></a></li>
-                <li id="li"><a href="https://www.callgirlsinlucknow.in/about-us/"><button>About Us</button></a></li>
-                <li id="li"><a href="https://www.callgirlsinlucknow.in/contact-us/"><button>Contact Us</button></a></li>
-                <li id="li"><a href="https://www.callgirlsinlucknow.in/contact-us/"><button>Post Ad</button></a></li>
+                <li id="li"><a href="https://callgirlsinlucknow.in/reviews/"><button>Reviews</button></a></li>
+                <li id="li"><a href="https://callgirlsinlucknow.in/gallery/"><button>Gallery</button></a></li>
+                <li id="li"><a href="https://callgirlsinlucknow.in/price-list/"><button>Price List</button></a></li>
+                <li id="li"><a href="https://callgirlsinlucknow.in/about-us/"><button>About Us</button></a></li>
+                <li id="li"><a href="https://callgirlsinlucknow.in/contact-us/"><button>Contact Us</button></a></li>
+                <li id="li"><a href="https://callgirlsinlucknow.in/contact-us/"><button>Post Ad</button></a></li>
                 
         </nav>
     </div>
-    <!-- https://www.callgirlsinlucknow.in/escorts-in-novotel-hotel/ -->
+    <!-- https://callgirlsinlucknow.in/escorts-in-novotel-hotel/ -->
